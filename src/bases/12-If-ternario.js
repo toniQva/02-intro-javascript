8cc576bd22ebf5a00f380c45bfae9cd4f395ac8d
Antonio Perez <tonypm2708@gmail.com>
2023-02-19 14:13:50 -0500

    Add if ternario

---
+++ src/bases/12-If-ternario.js
@@ -0,0 +1,12 @@
+const activo = true;
+
+
+
+
+// const mensaje = (activo) ? 'activo': 'Inactivo';
+// const mensaje = (!activo) ? 'Inactivo': 'Activo';
+const mensaje = activo && 'Activo';
+
+
+console.log(mensaje);
+
